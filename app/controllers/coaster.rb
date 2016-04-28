c778bbb32f5e4dcd2988940981063df61e62b366
Joshua Lugo & Wesley El-Amin <pair+joshulu.wesleye2003@devbootcamp.com>
2016-04-28 15:06:15 -0500

    Update unirest format

--- app/controllers/coaster.rb
+++ app/controllers/coaster.rb
@@ -1,7 +1,7 @@
 post "/blank" do
   response = Unirest.post 'https://life-left.p.mashape.com/time-left',
     headers: {
-      "X-Mashape-Authorization" => ENV["MASHAPE_KEY"],
+      "X-Mashape-Key" => ENV["MASHAPE_KEY"],
       "Content-Type" => "application/x-www-form-urlencoded",
       "Accept" => "application/json"
     },
@@ -9,9 +9,9 @@ post "/blank" do
       "birth" => params[:birth],
       "gender" => params[:gender]
     }
-    puts response.code
-    puts response.headers
-    puts response.body
+    # puts response.code
+    # puts response.headers
+    # puts response.body
     puts response.raw_body
 end
 
